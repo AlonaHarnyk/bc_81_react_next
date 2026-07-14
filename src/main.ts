7e48bafb935f17fb1d84f0633fa87be146d9e839
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-14 17:50:54 +0300

    02-ts-2 Maks Step 3nd time

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // // enum Role {
 // //   Admin = 'admin',
 // //   Guest = 'guest',
@@ -109,5 +111,111 @@
 // console.log(firstOrDefault<number>([]));
 // console.log(firstOrDefault<number>([undefined]));
 
+// // ============
 
-// ============
+interface User {
+  name: string;
+}
+
+// interface Responce<T> {
+//   status: number;
+//   message: string;
+//   data: T;
+// }
+
+// // interface TodoResponce {
+// //   status: number;
+// //   message: string;
+// //   data: User[];
+// // }
+
+// const todoResp: Responce<User[]> = {
+//   status: 200,
+//   message: "success",
+//   data: [{ name: "Alex" }, { name: "Vlad" }],
+// };
+
+// // interface OneTodoResponce {
+// //   status: number;
+// //   message: string;
+// //   data: User;
+// // }
+
+// const oneTodoResp: Responce<User> = {
+//   status: 200,
+//   message: "success",
+//   data: { name: "Alex" },
+// };
+
+// // =======================
+
+// // Задача 7. Узагальнена функція delay з Promise
+// // Умова
+
+// // Створи функцію delay, яка:
+// // приймає значення будь-якого типу
+// // приймає час у мілісекундах
+// // повертає Promise, який після затримки повертає передане значення
+// // зроби функцію узагальненою
+
+// function delay<T>(value: T, delayMs: number): Promise<T> {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(value);
+//     }, delayMs);
+//   });
+// }
+
+// delay<string>("hello", 1000).then((value) => {
+//   console.log(value);
+// });
+
+// // =======================
+
+// // Задача 8. Узагальнена функція fetchData з Axios
+// // Умова
+
+// // Створи функцію fetchData, яка:
+// // приймає URL
+// // робить GET-запит через axios
+// // повертає масив об’єктів певного типу, який визначається узагальненням
+
+// async function fetchData<T>(url: string): Promise<T[]> {
+//   const { data } = await axios.get<T[]>(url);
+//   return data;
+// }
+
+// fetchData<User>("example.com").then((data) => {
+//   console.log(data);
+// });
+
+// // =======================
+
+// Задача 9. Узагальнена функція zip
+// Умова
+// Створи функцію zip, яка:
+
+// приймає два масиви різного типу
+// повертає масив кортежів
+
+// якщо масиви різної довжини, ігнорує зайві елементи
+// зроби функцію узагальненою
+
+function zip<T, U>(arr1: T[], arr2: U[]): [T, U][] {
+  const len = Math.min(arr1.length, arr2.length);
+
+  const result: [T, U][] = [];
+
+  for (let i = 0; i < len; i += 1) {
+    result.push([arr1[i], arr2[i]]);
+  }
+
+  return result;
+}
+
+// Приклад використання:
+
+const numbers = [1, 2, 3];
+const strings = ["a", "b"];
+const zipped = zip(numbers, strings); // [[1,"a"], [2,"b"]]
+console.log("🚀 ~ zipped:", zipped);
