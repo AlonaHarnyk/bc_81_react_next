415c46b1c5d3fb00b450c9eab237912706909c5e
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-16 17:12:15 +0300

    03 Maks Step

--- src/components/Student/Student.tsx
+++ src/components/Student/Student.tsx
@@ -5,13 +5,15 @@ interface StudentProps {
 }
 
 export default function StudentItem({ student }: StudentProps) {
-
   return (
     <div>
       <img src={student.avatar} alt={student.name} />
       <h2>{student.name}</h2>
       <p>Age: {student.age}</p>
       <p>Course: {student.course}</p>
+      {/* <p>Is online: {student.isOnline ? "yes" : "no"}</p> */}
+      {/* {student.isOnline ? <p> Online </p> : <p> Offline </p>} */}
+      {student.isOnline && <p>User is online</p>}
     </div>
   );
 }
