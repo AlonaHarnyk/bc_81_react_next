fe0a1eb738e816049634effce0ddd804c67cc674
ZinVolodymyr <vl.zinych@gmail.com>
2026-07-17 16:23:44 +0300

    zinych 04

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-
-import { initialDogs } from "../../data/dogs";
-import DogsList from "../DogsList/DogsList";
+import { useState } from 'react';
+import Button from '../Button/Button';
+import { initialDogs } from '../../data/dogs';
+import DogsList from '../DogsList/DogsList';
 
 export default function App() {
   const [dogs, setDogs] = useState(initialDogs);
-
+  const [isDogListVisible, setIsDogListVisible] = useState(false);
+  const toggleShowDogList = () => {
+    setIsDogListVisible(!isDogListVisible);
+  };
   return (
-    <DogsList dogs={dogs} />
+    <>
+      <Button
+        type="button"
+        textContent={isDogListVisible ? 'Hide dogs list' : 'Show dog list'}
+        handleClick={toggleShowDogList}
+      />
+      {isDogListVisible && <DogsList dogs={dogs} />}
+    </>
   );
 }
