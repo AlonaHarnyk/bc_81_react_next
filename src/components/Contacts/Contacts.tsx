84765a7144794abe452fccb9a42b9fbfe52f26b9
ZinVolodymyr <vl.zinych@gmail.com>
2026-07-23 17:58:12 +0300

    react-query-Zinych-2

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,13 +1,34 @@
-import { useQuery } from "@tanstack/react-query"
-import { getContacts } from "../../services/contactsApi"
+import { useQuery } from '@tanstack/react-query';
+import { getContacts } from '../../services/contactsApi';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
 
-export default function Contacts(){
-    const {data, isError, isLoading} = useQuery({
-        queryKey: ['contacts'],
-        queryFn: getContacts,
-    })
-    console.log(data);
-    
-    return <p>Contacts</p>
+export default function Contacts() {
+  const {
+    data: contacts,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ['contacts'],
+    queryFn: getContacts,
+  });
+
+  return (
+    <>
+      <ul>
+        {contacts &&
+          contacts.length > 0 &&
+          contacts.map(contact => (
+            <li key={contact.id}>
+              <p>{contact.name}</p>
+              <p>{contact.city}</p>
+              <p>{contact.job}</p>
+              <p>{contact.email}</p>
+            </li>
+          ))}
+      </ul>
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+    </>
+  );
 }
-
